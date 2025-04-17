a9e6887f073499abd411b82f664cbdf2f63f7a3c
Moises12-3 <princesatitu12>
2025-04-17 13:58:38 -0600

    v

--- MyProfile.php
+++ MyProfile.php
@@ -276,7 +276,6 @@ $resultado = $stmt->get_result();
 $usuario = $resultado->fetch_assoc();
 ?>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
 
